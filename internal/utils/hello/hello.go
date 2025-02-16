a815b75e1ffe315e40376c730a098d6cd67b0646
tyrese-r <git@tbertie.com>
2025-02-16 15:27:00 +0000

    Updated config

--- internal/utils/hello/hello.go
+++ internal/utils/hello/hello.go
@@ -2,15 +2,10 @@ package hello
 
 import "fmt"
 
-var unusedVar = 42 // Unused variable
-
 func Hello(name string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if false {
-		fmt.Println("This will be printed")
-	}
-	return "Hello, " + name + "!"
+	return fmt.Sprintf("Hello, %s!", name)
 }
